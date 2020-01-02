143d010aaf4471b751ef8cf69d8c253a5b6d37e7
chibitofu <erinmoony@gmail.com>
2020-01-02 15:57:49 -0800

    Bot now listens and send back messages.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # discord-image-bot
+Install project npm modules
+npm install
+
+Create .env file and add your Discord bot token to it.
+ex. ehco "TOKEN=your-discord-token-goes-here" >> .env
+
+run bot locally using
+node app.js
+
+Discord.js guide - https://discordjs.guide/creating-your-bot
+Discord.js Documentation - https://discord.js.org/#/docs/main/stable/general/welcome
+Discord.js github - https://github.com/discordjs/discord.js
+dotenv Documentation - https://www.npmjs.com/package/dotenv
--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
+const token = process.env.DISCORD_TOKEN;
 
+// this event will only trigger one time after logging in
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('Image Bot Ready!');
 });
 
-client.login(process.env.DISCORD_TOKEN);
+// listens to all messages sent on the Discord server
+client.on('message', message => {
+    // listens for a specific word or phrase
+    if (message.content === '!ping') {
+        // sends back a message to the channel the original message was sent in
+        message.channel.send('Pong.');
+    }
+});
+
+
+
+// login to Discord with your app's token
+client.login(token);
